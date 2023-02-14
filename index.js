1d3b1013168a77cc76ee2a3df71094d07be77abb
pratikganjale55 <100259765+pratikganjale55@users.noreply.github.com>
2023-02-14 14:23:52 +0530

    port change

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const express= require('express');
-
-const connection= require("./src/database/db")
+const express = require("express");
+const connection = require("./src/database/db");
 const app = express();
-const cors= require('cors')
-const authRoute = require('./src/routes/user');
-const dotenv = require('dotenv');
-dotenv.config({path : "./src/config/.env"})
-app.use(express.urlencoded({ extended:true }));
+const cors = require("cors");
+const authRoute = require("./src/routes/user");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./src/config/.env" });
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use("/auth",authRoute)
+app.use("/auth", authRoute);
 
+app.get("/", (req, res) => {
+  res.send({ message: "welcome to our website" });
+});
 
-app.get("/",(req,res)=>{
-res.send({message:"welcome to our website"})
-})
-
-
- 
-
-app.listen(process.env.PORT,async()=>{
-    
-    await connection;
-console.log("listening on port 8080")
+app.listen(process.env.PORT, async () => {
+  await connection;
+  console.log(`listening on port ${process.env.PORT}`);
 });
