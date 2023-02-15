9ce53483670d440baeb89f863874707bd5adedf2
pratikganjale55 <100259765+pratikganjale55@users.noreply.github.com>
2023-02-15 13:01:23 +0530

    create branch

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
+require('dotenv').config();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const db = process.env.DATABASE; 
+
+
 const connection = mongoose.connect("mongodb+srv://masai-portal:masai-portal@cluster0.syawzk7.mongodb.net/masai-portal?retryWrites=true&w=majority")
-.then(() => console.log("Database successfully connect"))
-.catch((e) => console.log(e))
-module.exports = connection
+  .then(() => console.log("Database successfully connect"))
+  .catch((e) => console.log(e));
+
+
+
+module.exports = connection;
