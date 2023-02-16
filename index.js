6876588cdd6433e91835c6271c22cb46e055c7b5
pratikganjale55 <100259765+pratikganjale55@users.noreply.github.com>
2023-02-16 12:12:29 +0530

    modify route of comapny crud operation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", authRoute);
-app.use("/newCompany", companyRoute) ;
+app.use("/", companyRoute) ;
 
 app.get("/", (req, res) => {
   res.send({ message: "welcome to our website" });
