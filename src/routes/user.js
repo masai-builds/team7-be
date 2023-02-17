bb89a84365af5bcd5b4e8be80a9de524bd7485a6
pratikganjale55 <100259765+pratikganjale55@users.noreply.github.com>
2023-02-17 20:21:20 +0530

    resetPAssword done

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,7 +10,7 @@ const handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
 
-// signup 
+// signup
 
 authRoute.post("/signup", async (req, res) => {
   const userMail = await userModel.findOne({ email: req.body.email });
@@ -27,12 +27,10 @@ authRoute.post("/signup", async (req, res) => {
   }
 
   if (!passwordRegex.test(password)) {
-    return res
-      .status(400)
-      .send({
-        message:
-          "Password must contain at least 8 characters, including at least 1 number, 1 lowercase letter, and 1 uppercase letter.",
-      });
+    return res.status(400).send({
+      message:
+        "Password must contain at least 8 characters, including at least 1 number, 1 lowercase letter, and 1 uppercase letter.",
+    });
   }
 
   if (!emailReg.test(email)) {
@@ -77,27 +75,27 @@ authRoute.post("/signup", async (req, res) => {
       if (err) {
         return res.status(500).send({ message: "Error sending email" });
       }
-      return res.status(200).send({ message: " successfully signup with email" });
+      return res
+        .status(200)
+        .send({ message: " successfully signup with email" });
     });
-    return res
-      .status(201)
-      .send({ message: "successfully registered"});
+    return res.status(201).send({ message: "successfully registered" });
   });
 });
 
-// login 
+// login
 
-authRoute.post("/login", async (req, res,next) => {
+authRoute.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
   const validUser = await userModel.findOne({ email });
 
   if (!email || !password) {
-    res.status(422).send({ message: "fill all the details" })
+    res.status(422).send({ message: "fill all the details" });
   }
 
   if (!validUser) {
     return res.status(401).send({ message: "Invalid Credentials" });
-  } 
+  }
 
   const isMatch = await bcrypt.compare(password, validUser.password);
 
@@ -106,29 +104,31 @@ authRoute.post("/login", async (req, res,next) => {
   }
 
   // authorize based on user role
-  const authorizedRoles = ["Admin", "Student"]; 
-    if (authorizedRoles.length && !authorizedRoles.includes(validUser.role)) {
-        // user's role is not authorized
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+  const authorizedRoles = ["Admin", "Student"];
+  if (authorizedRoles.length && !authorizedRoles.includes(validUser.role)) {
+    // user's role is not authorized
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-  const token = jwt.sign({
+  const token = jwt.sign(
+    {
       name: validUser.name,
-    }, process.env.JWT_KEY);
+    },
+    process.env.JWT_KEY
+  );
 
-    // cookiegenerate
-    res.cookie("usercookie",token,{
-        expires:new Date(Date.now()+9000000),
-        httpOnly:true
-    });
+  // cookiegenerate
+  res.cookie("usercookie", token, {
+    expires: new Date(Date.now() + 9000000),
+    httpOnly: true,
+  });
 
   // authentication and authorization successful
   next();
   return res.status(201).send({ validUser, token });
-   
 });
 
-// forgetPassword 
+// forgetPassword
 
 authRoute.post("/forgetPassword", async (req, res) => {
   const { email } = req.body;
@@ -145,14 +145,13 @@ authRoute.post("/forgetPassword", async (req, res) => {
     expiresIn: "15m",
   });
 
-// Set up the email transporter 
+  // Set up the email transporter
 
   const directory = path.join(__dirname, "..", "utiles", "resetPassowrd.html");
   const fileRead = fs.readFileSync(directory, "utf-8");
   const template = handlebars.compile(fileRead);
-  const htmlToSend = template({ name: user.name, userId : user._id });
+  const htmlToSend = template({ name: user.name, userId: user._id });
 
-  
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -166,7 +165,7 @@ authRoute.post("/forgetPassword", async (req, res) => {
     from: process.env.USER_EMAIL,
     to: email,
     subject: "Password Reset Request",
-    html: htmlToSend
+    html: htmlToSend,
   };
 
   // Send the email
@@ -178,30 +177,30 @@ authRoute.post("/forgetPassword", async (req, res) => {
   });
 });
 
-//reset password 
+//reset password
 
-authRoute.patch("/resetPassword/:id",async(req,res)=>{
+authRoute.patch("/resetPassword/:id", async (req, res) => {
   const { id } = req.params;
-  const { password,rePassword } = req.body;
- 
+  const { password, rePassword } = req.body;
 
-  const oldUser = await userModel.findOne({ _id : id });
+  const oldUser = await userModel.findOne({ _id: id });
   if (!oldUser) {
     return res.json({ status: "User Not Exists!!" });
   }
   try {
-  const salt = await bcrypt.genSaltSync(10);
-  const Pass = await bcrypt.hash(password, salt);
-  const rePass = await bcrypt.hash(rePassword, salt);
+    const salt = await bcrypt.genSaltSync(10);
+    const Pass = await bcrypt.hash(password, salt);
+    const rePass = await bcrypt.hash(rePassword, salt);
 
-  const setNewPass =  await userModel.findByIdAndUpdate({ _id: id},{$set: { password: Pass , rePassword:rePass}});
-  setNewPass.save();
-    res.status(201).send({message: 'Password updated successfully'});
-    
+    const setNewPass = await userModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: { password: Pass, rePassword: rePass } }
+    );
+    setNewPass.save();
+    res.status(201).send({ message: "Password updated successfully" });
   } catch (error) {
-    
     res.json({ status: "Something Went Wrong" });
   }
-})
+});
 
 module.exports = authRoute;
